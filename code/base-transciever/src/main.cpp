16cccb41dc5653168c317e5173617325fc50249a
Leon Knauf <leon.knauf@gmail.com>
2023-09-08 23:27:23 +0200

    base transciever inital commit with http comm

---
+++ code/base-transciever/src/main.cpp
@@ -0,0 +1,90 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+#include "ESPAsyncWebServer.h"
+#include <String.h>
+
+#include <ESP8266WiFiMulti.h>
+ESP8266WiFiMulti WiFiMulti;
+
+AsyncWebServer server(80);
+
+const char* ssid = "NUT";
+const char* password = "123456789";
+
+unsigned long previousMillis = 0;
+const long interval = 5000; 
+
+String test;
+String link = "http://192.168.4.1/link1";
+
+String httpGETRequest(const char* serverName) {
+  WiFiClient client;
+  HTTPClient http;
+    
+  // Your IP address with path or Domain name with URL path 
+  http.begin(client, serverName);
+  
+  // Send HTTP POST request
+  int httpResponseCode = http.GET();
+  
+  String payload = "--"; 
+  
+  if (httpResponseCode>0) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    payload = http.getString();
+  }
+  else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+  // Free resources
+  http.end();
+
+  return payload;
+}
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println();
+  
+ 
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("Connected to WiFi: "+WiFi.localIP().toString());
+
+  server.on("/link2", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", "OK");
+    Serial.println("link2 recv");
+  });
+
+  server.begin();
+}
+
+void loop() {
+  /*
+  unsigned long currentMillis = millis();
+  
+  if(currentMillis - previousMillis >= interval) {
+     // Check WiFi connection status
+    if ((WiFiMulti.run() == WL_CONNECTED)) {
+      test = httpGETRequest(link.c_str());
+      Serial.println(test);
+      
+      
+      // save the last HTTP GET Request
+      previousMillis = currentMillis;
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+  }*/
+}
+
