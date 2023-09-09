4b2e91613e993c6957aabff1e24d8da9feb33e53
Leon Knauf <leon.knauf@gmail.com>
2023-09-09 16:48:24 +0200

    base-transciever valve control

--- code/base-transciever/src/main.cpp
+++ code/base-transciever/src/main.cpp
@@ -3,8 +3,9 @@
 #include <WiFiClient.h>
 #include "ESPAsyncWebServer.h"
 #include <String.h>
-
+#include <Servo.h>
 #include <ESP8266WiFiMulti.h>
+
 ESP8266WiFiMulti WiFiMulti;
 
 AsyncWebServer server(80);
@@ -20,6 +21,22 @@ const long interval = 5000;
 
 String test;
 String link = "http://192.168.4.1/link1";
+
+#define VALVE1_PIN 14
+#define VALVE2_PIN 12
+#define VALVE3_PIN 13
+#define VALVE4_PIN 15
+
+#define VALVE_POS_CLOSED 0
+#define VALVE_POS_OPEN 180
+
+Servo valve1, valve2, valve3, valve4;
+
+const int TrigSensPin = A0;
+bool foundVehicle = false;
+
+bool automaticMode = true;
+int automaticState = 0;
 
 String httpGETRequest(const char* serverName) {
   WiFiClient client;
@@ -48,6 +65,32 @@ String httpGETRequest(const char* serverName) {
   return payload;
 }
 
+void controlValves(String valveNum, String state) {
+  Servo valve;
+  if (strcmp(valveNum.c_str(), "valve1") == 0){
+    valve = valve1;
+  } else if (strcmp(valveNum.c_str(), "valve2") == 0){
+    valve = valve2;
+  } else if (strcmp(valveNum.c_str(), "valve3") == 0){
+    valve = valve3;
+  } else if (strcmp(valveNum.c_str(), "valve4") == 0){
+    valve = valve4;
+  } else {
+    Serial.println("Error parsing Command (valveNum)");
+    return;
+  }
+  
+  if (strcmp(state.c_str(), "open") == 0){
+    valve.write(VALVE_POS_OPEN);
+  } else if (strcmp(state.c_str(), "close") == 0){
+    valve.write(VALVE_POS_CLOSED);
+  } else {
+    Serial.println("Error parsing Command (state)");
+    return;
+  }
+  
+}
+
 void setup() {
   Serial.begin(9600);
   Serial.println();
@@ -66,15 +109,68 @@ void setup() {
   Serial.println("");
   Serial.println("Connected to WiFi: "+WiFi.localIP().toString());
 
-  server.on("/link2", HTTP_GET, [](AsyncWebServerRequest *request){
+  server.on("/valve", HTTP_GET, [](AsyncWebServerRequest *request){
+    String valveNum;
+    String state;
+    // GET input1 value on <ESP_IP>/control?command=<inputMessage1>&value=<inputMessage2>
+    if (request->hasParam("valveNum") && request->hasParam("state")) {
+      valveNum = request->getParam("valveNum")->value();
+      state = request->getParam("state")->value();
+    }
+    else {
+      valveNum = "No message sent";
+      state = "No message sent";
+    }
+    Serial.print("valveNum: ");
+    Serial.print(valveNum);
+    Serial.print(", Value: ");
+    Serial.println(state);
     request->send_P(200, "text/plain", "OK");
-    Serial.println("link2 recv");
+    controlValves(valveNum, state);
   });
 
   server.begin();
+
+  valve1.attach(VALVE1_PIN, 900, 1500);
+  valve2.attach(VALVE2_PIN, 900, 1500);
+  valve3.attach(VALVE3_PIN, 900, 1500);
+  valve4.attach(VALVE4_PIN, 900, 1500);
+
+  valve1.write(VALVE_POS_CLOSED);
+  valve2.write(VALVE_POS_CLOSED);
+  valve3.write(VALVE_POS_CLOSED);
+  valve4.write(VALVE_POS_CLOSED);
+
 }
 
 void loop() {
+  String result;
+  foundVehicle = analogRead(TrigSensPin) < 100;
+  
+  if (automaticMode) {
+    switch (automaticState) {
+      case 0: //wait for vehicle to be in range
+        if (foundVehicle) {
+          automaticState = 10;
+        }
+        break;
+
+      case 10: //tell vehicle to follow line
+        result = httpGETRequest("http://192.168.4.1/control?command=automatic&value=1");
+        Serial.println(result);
+        automaticState = 20;
+        break;
+
+      case 20: //wait until vehicle leaves the sensor
+        if (!foundVehicle) {
+          automaticState = 0;
+        }
+        break;
+
+    } 
+  } else {
+    automaticState = 0;
+  }
   /*
   unsigned long currentMillis = millis();
   
